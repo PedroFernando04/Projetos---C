71aab5c24c587a820e08ed6fdb63de05995b5e60
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-16 14:35:13 -0300

    Create Igual_ou_Diferente.c

---
+++ 2 periodo/Huxley/Igual_ou_Diferente.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    char array[5];
+    int numero = 0;
+    
+    for(int i = 0; i < 3; i++)
+    {
+        scanf("%d", &numero);
+        array[i] = numero;
+    }
+    
+    if(array[0] == array[1] && array[1] == array[2])
+    {
+        printf("%d", 1);
+    }
+    
+    else if(array[0] != array[1] && array[0] != array[2] && array[1] != array[2])
+    {
+        printf("%d", 2);
+    }
+    
+    else
+        printf("%d", 3);
+    return 0;
+}
